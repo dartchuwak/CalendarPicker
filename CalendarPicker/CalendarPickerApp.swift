07aed01b5480ed8fe68d83923528d02eb71116b7
Evgenii Mikhailov <e.mikhailov@outlook.com>
2024-03-13 16:05:38 +0300

    Initial Commit

---
+++ CalendarPicker/CalendarPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarPickerApp.swift
+//  CalendarPicker
+//
+//  Created by Evgenii Mikhailov on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalendarPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
